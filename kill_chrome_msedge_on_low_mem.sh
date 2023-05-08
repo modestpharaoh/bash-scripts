2fdabb026792147d1c9044c27880e8cee51e7289
modestpharaoh <30378999+modestpharaoh@users.noreply.github.com>
2023-05-09 00:23:27 +0100

    Create kill_chrome_msedge_on_low_mem.sh

---
+++ kill_chrome_msedge_on_low_mem.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+  overloaded=$(free -m | awk 'NR==2{if ($3*100/$2 > 85) printf "1"; else printf "0";}')
+  used_mem=$(free -m | awk 'NR==2{printf "%.2f%%\n", $3*100/$2 }')
+  now=$(date)
+  if (( ${overloaded} )); then
+    echo "${now}: Overloaded memory detected: ${used_mem}, trying to kill fatest Chrome/Edge tab..."
+  
+    # Find chrome/msedge tab process that eat most memory
+    pid=$(ps -C chrome,msedge  -o pid,user,%mem,cmd --sort=%mem --no-header | grep 'renderer' | grep -v extension | tail -n 1 | awk '{print $1}')
+  
+    # Kill that process process
+    if [ ! -z "$pid" ]; then
+      echo "Killing Chrome/Edge tab process with PID $pid"
+      kill "$pid"
+    else
+      echo "No Chrome/Edge tab processes found"
+    fi
+
+  else
+    echo "${now}: Mem usage: ${used_mem}"
+  fi
+  sleep 60
+done
